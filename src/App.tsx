e0fd6f3b0705b428674240e7eb10871dccfe0c21
joaopedroAnelli <joaopedroanelli99@gmail.com>
2024-02-19 13:36:38 -0300

    Refactor App component to use ModalProvider and ModalListener

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppBody from './AppBody';
+import ModalListener from './ModalListener';
+import ModalProvider from './ModalProvider';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ModalProvider>
+      <div id='modal-root'></div>
+
+      <AppBody />
+
+      <ModalListener />
+    </ModalProvider>
   );
 }
 
